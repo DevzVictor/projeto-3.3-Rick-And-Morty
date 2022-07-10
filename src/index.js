b37e1684bfd61f1a7f67d1fa01283d0290e273fd
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2022-07-10 11:11:17 -0300

    backend initial config

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+const app = express();
+const routes = require('./routes/personagens.routes');
+const connectToDataBase = require('./database/database');
+
+connectToDataBase();
+
+app.use(express.json());
+app.use(cors());
+app.use('/personagens', routes);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
