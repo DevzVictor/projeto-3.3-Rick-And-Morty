51e19054794f7a6e8b952d55eb49201964d64d6c
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2022-07-14 21:15:08 -0300

    create users routes

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 3001;
 const app = express();
-const routes = require('./routes/characters.routes');
-const swaggerRoute = require('./swagger/swagger.route');
+const characterRoutes = require('./routes/characters.routes');
+const swaggerRoutes = require('./swagger/swagger.route');
+const userRoutes = require('./routes/users.routes');
 const connectToDataBase = require('./database/database');
 
 connectToDataBase();
 
 app.use(express.json());
 app.use(cors());
-app.use('/characters', routes);
-app.use('/api-docs', swaggerRoute);
+app.use('/characters', characterRoutes);
+app.use('/api-docs', swaggerRoutes);
+app.use('/users', userRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
